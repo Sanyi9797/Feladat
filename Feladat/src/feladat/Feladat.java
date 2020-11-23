cd5ff1a3e2d30d55d9c23612646316e085196903
Felhasználó <Felhasznl@Asus>
2020-11-23 21:31:59 +0100

    A program bekér egy számot és 0-tól addig a számíg kiírja a páros és páratlan számokat külön.

---
+++ Feladat/src/feladat/Feladat.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feladat;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Felhasználó
+ */
+public class Feladat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        int n = 0;
+        
+        Scanner beker = new Scanner(System.in);
+        
+        System.out.print("Kérem adja meg 'n' értékét:  ");
+        n = beker.nextInt();
+         
+        
+        System.out.println("A páros számok: ");
+        for ( i = 0; i <= n; i++  ){
+            
+            if ( i % 2 == 0 ){
+                
+                System.out.print( i + ", ");
+                
+            }
+            
+        }
+        
+        System.out.println("\n");
+        
+        System.out.println("A páratlan számok: ");
+        for ( i = 0; i <= n; i++  ){
+            
+            if ( i % 2 >= 1 ){
+                
+                System.out.print( i + ", ");
+                
+            }
+            
+        }
+        
+        System.out.println("\n");
+    }
+    
+}
